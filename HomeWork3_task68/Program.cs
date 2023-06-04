90c2b873823e9c82f513aa570895f44b947917a1
Alexey_mik <mikmav.mik@gmail.com>
2023-06-04 14:42:32 +0300

    task68 функция Аккермана

---
+++ HomeWork3_task68/Program.cs
@@ -0,0 +1,34 @@
+﻿// Задача 68: Напишите программу вычисления функции Аккермана с помощью рекурсии. 
+// Даны два неотрицательных числа m и n.
+// m = 2, n = 3 -> A(m,n) = 9
+// m = 3, n = 2 -> A(m,n) = 29
+//
+void Task68()
+{
+    System.Console.WriteLine("Задача 68: функция Аккермана A(m, n)");
+    int m = ReadInt("Введите m:");
+    int n = ReadInt("Введите n:");
+
+    if (m < 0 || n < 0)
+    {
+        System.Console.WriteLine($"Недопустимые значения (m, n)");
+        return;
+    }
+    System.Console.WriteLine($"Функция Аккермана={Accerman(m, n)}");
+}
+
+int ReadInt(string text)
+{
+    System.Console.WriteLine(text);
+    return Convert.ToInt32(Console.ReadLine());
+}
+
+int Accerman(int m, int n)
+{
+    if (m == 0) return n + 1;
+    if (n == 0) return Accerman(m - 1, 1);
+    return Accerman(m - 1, Accerman(m, n - 1));
+
+}
+
+Task68();
