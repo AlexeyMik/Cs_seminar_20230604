3fe93f290bfa8043f3c87fa4d86af3514803bc9a
Alexey_mik <mikmav.mik@gmail.com>
2023-06-04 13:57:57 +0300

    task66 сумма чисел из диапазона

---
+++ HomeWork2_task66/Program.cs
@@ -0,0 +1,30 @@
+﻿// Задача 66: Задайте значения M и N. Напишите программу, которая найдёт сумму натуральных элементов 
+// в промежутке от M до N.
+// M = 1; N = 15 -> 120
+// M = 4; N = 8. -> 30
+//
+// 
+void Task66()
+{
+    System.Console.WriteLine("Задача 66");
+    int m = ReadInt("Введите натуральное число");
+    int n = ReadInt("Введите натуральное число");
+    if (m <= n)
+        System.Console.WriteLine($"Сумма чисел= {SumOfAllBetween(m, n)}");
+    else
+        System.Console.WriteLine($"Сумма чисел= {SumOfAllBetween(n, m)}"); 
+}
+
+int ReadInt(string text)
+{
+    System.Console.WriteLine(text);
+    return Convert.ToInt32(Console.ReadLine());
+}
+
+int SumOfAllBetween(int m, int n)
+{
+    if (n < m) return 0; // нейтральное по отношению к сложению
+    return n + SumOfAllBetween(m, n - 1);
+}
+
+Task66();
